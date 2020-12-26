d4211cca700c8ad74ce0e82c3e1309fecd5a2f6f
Tanya Yang <tanyang@mit.edu>
2020-12-26 14:06:15 -0800

    fixed linkedin and made things more fungit add .

--- src/pages/doodles.js
+++ src/pages/doodles.js
@@ -1,6 +1,8 @@
 import React from "react"
 import Header from "../components/header"
 import Container from "../components/container"
+import welcome from "../images/doodles/welcome.png"
+import doodles from "../images/doodles/doodles.png"
 
 import aboutMe from "../images/doodles/about-me.jpg"
 import jrFall from "../images/doodles/jrfall.jpg"
@@ -10,11 +12,14 @@ import maineFall19 from "../images/doodles/maine-fall19.jpg"
 
 import acadiaSummer19 from "../images/doodles/acadia-summer19.jpg"
 import sfSummer19 from "../images/doodles/sf-summer19.jpg"
-import homeSpring19 from "../images/doodles/home-spring19.jpg"
+// import homeSpring19 from "../images/doodles/home-spring19.jpg"
 import bigFattiesSummer18 from "../images/doodles/bigfatties-summer18.jpg"
 import seattleSummer18 from "../images/doodles/seattle-summer18.jpg"
 
-
+const welcomeStyling = {
+  width: '45%',
+  minWidth: '45%'
+}
 export default function Doodles() {
   let slideIndex = 1;
   function plusDivs(n) {
@@ -33,6 +38,8 @@ export default function Doodles() {
   }
   return (
     <Header>
+              <img src={welcome} style={welcomeStyling}/>
+              <img src={doodles} id="doodles"/>
 
       <Container>
         {/* <div class="doodle-body">
@@ -42,8 +49,6 @@ export default function Doodles() {
           on top of them in notability. i'm personally inspired by color, so this is a helpful meditative
           hobby to ground myself.
         </p>
-
-        
         </div> */}
 
         <p> you've made it to the doodles page! currently figuring out a better way to display
@@ -59,7 +64,7 @@ export default function Doodles() {
         <img src={maineFall19} class="img-doodle"/>
         <img src={acadiaSummer19} class="img-doodle"/>
         <img src={sfSummer19} class="img-doodle"/>
-        <img src={homeSpring19} class="img-doodle"/>
+        {/* <img src={homeSpring19} class="img-doodle"/> */}
 
         <p> from 2018 below! </p>
         <img src={bigFattiesSummer18} class="img-doodle"/>
